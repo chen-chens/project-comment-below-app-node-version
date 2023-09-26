8b5114106c4dd216646ce61cd3cbaef5389eb731
Joanna.chen <joanna.chen@ewinsonic.com>
2023-09-26 17:03:59 +0800

    feat: practice node version

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
-const path = require('path');
+// const path = require('path');
 const template = require("art-template");
 
 
